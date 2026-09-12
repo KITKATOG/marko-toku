dd005326c6fc7e9958b99e42ad165e22ff95363d
Alpha <anascherkalaa@gmail.com>
2026-09-12 12:00:25 +0530

    Update version number to 5.4.7

--- beta.user.js
+++ beta.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Arbpay Sniper v5.0
 // @namespace    http://tampermonkey.net/
-// @version      5.4.6
+// @version      5.4.7
 // @description  Auto buyer
 // @author       Xolo
 // @match        https://arbpay.me/*
